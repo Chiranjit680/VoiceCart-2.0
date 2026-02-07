2cc774158678446a0044c3d469c513090d501f22
Anvesh Shrivastava <anveshshrivastava2095@gmail.com>
2026-02-07 10:07:30 +0530

    agent main.py

--- packages/src/agent/main.py
+++ packages/src/agent/main.py
@@ -1,23 +1,22 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from langchain_core.messages import BaseMessage, HumanMessage
 from agent_main import AgentExecutor
 import uvicorn
+
 app = FastAPI()
 
-class AgentRequest(BaseModel):
-    msg: str
-
 @app.post("/agent/{user_id}")
-async def agent_endpoint(user_id: int, body: AgentRequest):
-    try:            
-        response = await AgentExecutor().invoke(body.msg)
+async def agent_endpoint(user_id: int, body: dict):
+    try:
+        msg = body.get("msg")
+        if not msg:
+            raise ValueError("Missing 'msg' field in request body")
+        
+        response = AgentExecutor().invoke(msg)
         return {"response": response}
     except Exception as e:
+        print(f"Error in Agent Endpoint: {e}")
         raise HTTPException(status_code=500, detail=str(e))
         
         
 if __name__ == "__main__":
-        uvicorn.run(app, host="0.0.0.0", port=8002)
-        
-    #8002 for Agent microservice
+    uvicorn.run(app, host="0.0.0.0", port=8002)
