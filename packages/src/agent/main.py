c1c76ab0b69dc9d52bce8bf4344bb9656f66e61e
Chiranjit680 <chiranjitsaha680@gmail.com>
2026-02-07 06:39:50 +0530

    changed alot of things and fixed bugs

--- packages/src/agent/main.py
+++ packages/src/agent/main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from langchain_core.messages import BaseMessage, HumanMessage
 from agent_main import AgentExecutor
 import uvicorn
 app = FastAPI()
 
+class AgentRequest(BaseModel):
+    msg: str
+
 @app.post("/agent/{user_id}")
-async def agent_endpoint(msg: str):
+async def agent_endpoint(user_id: int, body: AgentRequest):
     try:            
-        response = await AgentExecutor().invoke(msg)
+        response = await AgentExecutor().invoke(body.msg)
         return {"response": response}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
         
         
 if __name__ == "__main__":
-        uvicorn.run(app, host="0.0.0.0", port=8067)
+        uvicorn.run(app, host="0.0.0.0", port=8002)
         
-    
+    #8002 for Agent microservice
